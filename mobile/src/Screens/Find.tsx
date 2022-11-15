fa135cae859d1c7c6a94e23f821f527ba4f7bf2a
Pedro Nery <pedrohnery@gmail.com>
2022-11-15 01:13:28 -0300

    Details

--- mobile/src/Screens/Find.tsx
+++ mobile/src/Screens/Find.tsx
@@ -1,17 +1,29 @@
+import { useState } from "react";
 import { Heading, Text, VStack } from "native-base";
-import { Header } from "../components/Header";
+
 import Logo from "../assets/logo.svg";
+
+import { Header } from "../components/Header";
 import { Input } from "../components/Input";
 import { Option } from "../components/Option";
 import { Button } from "../components/Button";
+import { isLoaded } from "expo-font";
 export function Find() {
+  async function handleJoinPool() {
+    const [isLoading, setIsLoading] = false;
+
+    try {
+      setIsLoading(true);
+    } catch (error) {
+    } finally {
+      setIsLoading(false);
+    }
+  }
   return (
     <VStack flex={1} bgColor="gray.900">
       <Header title="Criar novo bolão" showBackButton />
 
       <VStack mb={8} mx={5} alignItems="center">
-
-
         <Heading
           fontFamily={"heading"}
           color="white"
@@ -19,11 +31,14 @@ export function Find() {
           my={8}
           textAlign="center"
         >
-          Encontre um bolão através de{'\n'}seu código único
+          Encontre um bolão através de{"\n"}seu código único
         </Heading>
         <Input mb={2} placeholder="Qual o código do bolão??" />
-        <Button title="BUSCAR BOLÃO" />
-
+        <Button
+          title="BUSCAR BOLÃO"
+          isLoading={isLoading}
+          onPress={handleJoinPool}
+        />
       </VStack>
     </VStack>
   );
