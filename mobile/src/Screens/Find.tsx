a4d8d4bf8d73bb53b9370f25cdf07ef0749ed992
Pedro Nery <pedrohnery@gmail.com>
2022-11-15 01:14:06 -0300

    Inicio de Details

--- mobile/src/Screens/Find.tsx
+++ mobile/src/Screens/Find.tsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { Heading, Text, VStack } from "native-base";
+import { Heading, Text, VStack, useToast } from "native-base";
 
 import Logo from "../assets/logo.svg";
 
@@ -8,16 +8,49 @@ import { Input } from "../components/Input";
 import { Option } from "../components/Option";
 import { Button } from "../components/Button";
 import { isLoaded } from "expo-font";
+import { api } from "../services/api";
+import { useNavigation } from "@react-navigation/native";
 export function Find() {
-  async function handleJoinPool() {
-    const [isLoading, setIsLoading] = false;
+  const [isLoading, setIsLoading] = useState(false);
+  const [code, setCode] = useState("");
+  const {navigate} = useNavigation()
+  const toast = useToast();
 
+  async function handleJoinPool() {
     try {
       setIsLoading(true);
+
+      if (!code.trim()) {
+        return toast.show({
+          title: "Informe o código!",
+          placement: "top",
+          bgColor: "red.500",
+        })
+      };
+      api.post('/pools/join',{code})
+      navigate('pools')
+
+
+
+
     } catch (error) {
-    } finally {
+      console.log(error);
       setIsLoading(false);
-    }
+      if (error.response?.data?.messsage === "Pool not found!") {
+        return toast.show({
+          title: "Bolão não encontrado!",
+          placement: "top",
+          bgColor: "red.500",
+        });
+      }
+      if (error.response?.data?.messsage === "You already joined this pool!") {
+        return toast.show({
+          title: "você já esta neste Bolão!",
+          placement: "top",
+          bgColor: "red.500",
+        });
+      }
+    } 
   }
   return (
     <VStack flex={1} bgColor="gray.900">
@@ -33,7 +66,12 @@ export function Find() {
         >
           Encontre um bolão através de{"\n"}seu código único
         </Heading>
-        <Input mb={2} placeholder="Qual o código do bolão??" />
+        <Input
+          mb={2}
+          placeholder="Qual o código do bolão??"
+          autoCapitalize="characters"
+          onChangeText={setCode}
+        />
         <Button
           title="BUSCAR BOLÃO"
           isLoading={isLoading}
--- mobile/src/Screens/New.tsx
+++ mobile/src/Screens/New.tsx
@@ -1,10 +1,53 @@
-import { Heading, Text, VStack } from "native-base";
+import { useState } from "react";
+import { Heading, Text, VStack, useToast, Toast } from "native-base";
 import { Header } from "../components/Header";
 import Logo from "../assets/logo.svg";
 import { Input } from "../components/Input";
 import { Option } from "../components/Option";
 import { Button } from "../components/Button";
+import { Alert } from "react-native";
+import { api } from "../services/api";
 export function New() {
+  const [title, setTitle] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+  async function handlePoolCreate() {
+    if (!title.trim()) {
+      // Alert.alert('Novo bolão', 'informe o nome para seu bolão')
+      return Toast.show({
+        title: "Informe um nome para seu bolão",
+        placement: "top",
+        bgColor: "red.500",
+      });
+    }
+
+    try {
+      setIsLoading(true);
+      await api.post('/pools',{title})
+      Toast.show({
+        title: "Bolão Criado com sucesso!",
+        placement: "top",
+        bgColor: "green.500",
+
+      });
+
+   
+
+    } catch (error) {
+      console.log(error);
+
+      Toast.show({
+        title: "Não foi possivel criar bolão",
+        placement: "top",
+        bgColor: "red.500",
+      });
+    } finally{
+      setIsLoading(false);
+      setTitle('')
+    }
+
+
+  }
+
   return (
     <VStack flex={1} bgColor="gray.900">
       <Header title="Criar novo bolão" showBackButton />
@@ -21,9 +64,18 @@ export function New() {
         >
           Crie seu próprio bolão da copa e compartilhe entre amigos!
         </Heading>
-        <Input mb={2} placeholder="Qual nome do seu bolão?" />
-        <Button title="CRIAR MEU BOLÂO" />
-        <Text color="gray.200"  fontSize="sm" alignItems="center" px={10} mt={4}>
+        <Input
+          mb={2}
+          placeholder="Qual nome do seu bolão?"
+          onChangeText={setTitle}
+          value={title}
+        />
+        <Button
+          title="CRIAR MEU BOLÂO"
+          onPress={handlePoolCreate}
+          isLoading={isLoading}
+        />
+        <Text color="gray.200" fontSize="sm" alignItems="center" px={10} mt={4}>
           Após criar seu bolão, você receberá um código único que poderá usar
           para convidar outras pessoas.
         </Text>
